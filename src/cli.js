4ebb574163ed3e50725af8744b93d1731a3969e6
Andreas Holstenson <a@holstenson.se>
2015-07-17 09:26:17 +0200

    Refactored to make all devices and tags the same

--- src/cli.js
+++ src/cli.js
@@ -4,10 +4,6 @@ const chalk = require('chalk');
 const tinkerhub = require('tinkerhub');
 const out = require('./log');
 
-const allDevices = tinkerhub.devices.collection(function() {
-    return true;
-});
-
 if(process.argv.length > 2) {
     handleCommand(process.argv.slice(2), res => {
         if(res) {
@@ -45,20 +41,23 @@ if(process.argv.length > 2) {
 }
 
 function handleCommand(args, next) {
+    let devices;
     switch(args[0]) {
         case 'devices':
-            handleDevices(args, next);
+            devices = tinkerhub.devices.all();
             break;
         default:
-            handleDevice(args, next);
+            devices = tinkerhub.devices.tagged.apply(tinkerhub.devices, args[0].split(','));
             break;
     }
+
+    handleDeviceInvocations(devices, args, next);
 }
 
 function withDevices(devices, func) {
     if(typeof devices === 'function') {
         func = devices;
-        devices = allDevices;
+        devices = tinkerhub.devices.all();
     }
 
     if(devices.length > 0) {
@@ -69,9 +68,67 @@ function withDevices(devices, func) {
 }
 
 
-function handleDevices(args, next) {
-    printDeviceList(allDevices, next);
+function handleDeviceInvocations(devices, args, next) {
+    withDevices(devices, () => {
+        if(devices.length === 0) {
+            out.error('No devices matching', chalk.gray(args[0]));
+            return next(false);
+        }
+
+        if(args[1] === 'metadata') {
+            if(args[2] === 'tag') {
+                if(args[3]) {
+                    devices.metadata.tag(args[3])
+                        .then(() => next(true))
+                        .fail(() => next(false))
+                        .done();
+                } else {
+                    out.error('No tag specified');
+                    return next(false);
+                }
+            } else {
+                out.error('Don\'t know what to do with metadata');
+                return next(false);
+            }
+        } else if(args[1]) {
+            out.info('Invoking', chalk.gray(args[1]), 'on', devices.length, devices.length === 1 ? 'device' : 'devices');
+
+            const action = devices[args[1]];
+            action.apply(devices, args.slice(2))
+                .then((result) => {
+                    Object.keys(result).forEach((key) => {
+                        const data = result[key];
+                        if(data.error) {
+                            out.info(chalk.bgRed.white(' ERROR '), key);
+                            out.group();
+                            out.info(data.error.message);
+                            out.groupEnd();
+                        } else {
+                            out.info(chalk.bgGreen.white(' SUCCESS '), key);
+                            if(typeof data.value !== 'undefined') {
+                                out.group();
+                                out.info(data.value);
+                                out.groupEnd();
+                            }
+                        }
+                    });
+
+                    next(true);
+                })
+                .fail(() => next(false))
+                .progress(data => {
+                    out.info(chalk.bgWhite.black(' PROGRESS '), data.device);
+                    out.group();
+                    out.info(data.progress);
+                    out.groupEnd();
+                })
+                .done();
+        } else {
+            printDeviceList(devices, next);
+        }
+    });
 }
+
 
 function coloredTagList(tags) {
     return tags.map(tag => {
@@ -120,67 +177,5 @@ function printDeviceList(devices, next) {
         out.info('Found', devices.length, devices.length === 1 ? 'device' : 'devices');
 
         if(next) next();
-    });
-}
-
-function handleDevice(args, next) {
-    const device = tinkerhub.devices.tagged.apply(tinkerhub.devices, args[0].split(','));
-    withDevices(device, () => {
-        if(device.length === 0) {
-            out.error('No devices matching', chalk.gray(args[0]));
-            return next(false);
-        }
-
-        if(args[1] === 'metadata') {
-            if(args[2] === 'tag') {
-                if(args[3]) {
-                    device.metadata.tag(args[3])
-                        .then(() => next(true))
-                        .fail(() => next(false))
-                        .done();
-                } else {
-                    out.error('No tag specified');
-                    return next(false);
-                }
-            } else {
-                out.error('Don\'t know what to do with metadata');
-                return next(false);
-            }
-        } else if(args[1]) {
-            out.info('Invoking', chalk.gray(args[1]), 'on', device.length, device.length === 1 ? 'device' : 'devices');
-
-            const action = device[args[1]];
-            action.apply(device, args.slice(2))
-                .then((result) => {
-                    Object.keys(result).forEach((key) => {
-                        const data = result[key];
-                        if(data.error) {
-                            out.info(chalk.bgRed.white(' ERROR '), key);
-                            out.group();
-                            out.info(data.error.message);
-                            out.groupEnd();
-                        } else {
-                            out.info(chalk.bgGreen.white(' SUCCESS '), key);
-                            if(typeof data.value !== 'undefined') {
-                                out.group();
-                                out.info(data.value);
-                                out.groupEnd();
-                            }
-                        }
-                    });
-
-                    next(true);
-                })
-                .fail(() => next(false))
-                .progress(data => {
-                    out.info(chalk.bgWhite.black(' PROGRESS '), data.device);
-                    out.group();
-                    out.info(data.progress);
-                    out.groupEnd();
-                })
-                .done();
-        } else {
-            printDeviceList(device, next);
-        }
     });
 }
